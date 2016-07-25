29b2abb942cd9b2923f59b4eb02966d38d8c46c7
Gowrishankar <hiiamgowri@gmail.com>
2016-07-25 15:43:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
 
 app.get('/hello', function(req, res){
-  res.send('hello world');
+  res.send("result:[{app:"name", std:"hgfhgfhgfh"},{app:"name", std:"hgfhgfhgfh"},{app:"name", std:"hgfhgfhgfh"},{app:"name", std:"hgfhgfhgfh"}]");
 });
 
 app.listen(8081);
